012d3fb2748a62f843fab6e45be7f8e60b599185
Михаил З <mixaz@bk.ru>
2020-09-07 16:52:07 +0300

    border

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -7,48 +8,52 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-6 coffee-list">
+            <div class="col-5 coffee-list">
                 <div class="coffee-item">
                     <img src="img/espresso.png" alt="Эспрессо">
                     <span>Эспрессо - 45 руб. </span>
-                    </div>
+                </div>
                 <div class="coffee-item">
                     <img src="img/americano.png" alt="Американо">
                     <span>Американо - 56 руб. </span>
-                    </div>
+                </div>
                 <div class="coffee-item">
                     <img src="img/cappuccino.png" alt="Капучино">
                     <span>Капучино - 91 руб. </span>
-                    </div>
+                </div>
                 <div class="coffee-item">
                     <img src="img/latte.png" alt="Латте">
                     <span>Латте - 113 руб. </span>
+                </div>
+            </div>
+            <div class="col-7 coffee-operation">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="display"></div>
+                        <img src="" alt="">
                     </div>
-            </div>
-            <div class="col-6 coffee-operation">
-            <div class="row">
-                <div class="col-6">
-                    <div class="display"></div>
+                    <div class="col-6">
+                        <input type="text">
+                        <img src="" alt="">
+                        <button>Сдача</button>
+                        <div class="change"></div>
+                    </div>
                 </div>
-                <div class="col-6">
-                    <input type="text">
-                    <img src="" alt="">
-                    <button>Сдача</button>
-                </div>
-            </div>
             </div>
 
         </div>
     </div>
-    
-    
-    
-<script src="js/script.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+
+    <script src="js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
+
 </html>
