26e30bb955fa93df33ca134759d1977d55028b35
Mixaz <mixaz@bk.ru>
2020-09-07 12:41:04 +0300

    Сверстали кнопки кофемашины

--- index.php
+++ index.php
@@ -2,11 +2,49 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Coffe mashine</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 coffee-list">
+                <div class="coffee-item">
+                    <img src="img/espresso.png" alt="Эспрессо">
+                    <span>Эспрессо - 45 руб. </span>
+                    </div>
+                <div class="coffee-item">
+                    <img src="img/americano.png" alt="Американо">
+                    <span>Американо - 56 руб. </span>
+                    </div>
+                <div class="coffee-item">
+                    <img src="img/cappuccino.png" alt="Капучино">
+                    <span>Капучино - 91 руб. </span>
+                    </div>
+                <div class="coffee-item">
+                    <img src="img/latte.png" alt="Латте">
+                    <span>Латте - 113 руб. </span>
+                    </div>
+            </div>
+            <div class="col-6 coffee-operation">
+            <div class="row">
+                <div class="col-6">
+                    <div class="display"></div>
+                </div>
+                <div class="col-6">
+                    <input type="text">
+                    <img src="" alt="">
+                    <button>Сдача</button>
+                </div>
+            </div>
+            </div>
+
+        </div>
+    </div>
+    
+    
     
 <script src="js/script.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
