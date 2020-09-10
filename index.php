154a425762df747c90f8c275ca3b372bf7704c1c
Mixaz <mixaz@bk.ru>
2020-09-10 12:37:34 +0300

    Начали писать JS для кофемашины

--- index.php
+++ index.php
@@ -11,19 +11,19 @@
       <div class="container">
          <div class="row">
             <div class="col-12 col-sm-5 coffee-list">
-               <div class="coffee-item">
+               <div class="coffee-item" onclick="makeCofee('Эспрессо', 45)">
                   <img src="img/espresso.png" alt="Эспрессо">
                   <span>Эспрессо - 45 руб. </span>
                </div>
-               <div class="coffee-item">
+               <div class="coffee-item" onclick="makeCofee('Американо', 56)">
                   <img src="img/americano.png" alt="Американо">
                   <span>Американо - 56 руб. </span>
                </div>
-               <div class="coffee-item">
+               <div class="coffee-item" onclick="makeCofee('Капучино', 91)">
                   <img src="img/cappuccino.png" alt="Капучино">
                   <span>Капучино - 91 руб. </span>
                </div>
-               <div class="coffee-item">
+               <div class="coffee-item" onclick="makeCofee('Латтэ', 113)">
                   <img src="img/latte.png" alt="Латте">
                   <span>Латте - 113 руб. </span>
                </div>
@@ -62,6 +62,7 @@
          </div>
       </div>
       <script src="js/script.js"></script>
+      <script src="../8/js/script.js"></script>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+"use strict";
+
+function changeDisplayText(message) {
+    let displayText = document.querySelector('.display-text');
+    displayText.innerHTML = message;
+}
+
+function makeCofee(name, price) {
+    let balance = document.querySelector('.form-control');
+    if (Number(balance.value) >= price) {
+        balance.value -= price;
+        balance.style.backgroundColor = '';
+        changeDisplayText(`Ваш ${name} готовится`);
+    }else {
+        changeDisplayText("Недостаточно средств");
+        balance.style.backgroundColor = '#F1948A';
+    }
+    
+}
